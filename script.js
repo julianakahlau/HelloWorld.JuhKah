e646c31385c755d3ea26b615cb6eb1827a03cb60
julianakahlau <juliana.kahlau@escola.pr.gov.br>
2025-07-01 20:38:20 -0300

    console

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ function inserirNome() {
 }
 inserirNome();
 
-let linguagens = {"Javascript", "Python", "C"};
+let linguagens = ["Javascript", "Python", "C"];
+console.log(linguagens[0]);
+console.log(linguagens[1]);
+console.log(linguagens[2]);
 const item = document.querySelector("#lista");
 item.textContent = linguagens[2];
